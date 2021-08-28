0f1c13d01340f49206d0f1ecce9ae01ad2473e48
gsilvaro11 <guiuser1999@gmail.com>
2021-08-28 16:19:46 -0300

    Adicionando função atualizar_produtos

--- script/app.js
+++ script/app.js
@@ -7,7 +7,6 @@ let Produtos = [
 ]
 
 
-//Pega os valores do front e valida e passa para as funções
 const valida_produtos = () => {
     let nome_produto = document.getElementById('envia_nome').value
     let preco_produto = document.getElementById('envia_preco').value
@@ -19,6 +18,26 @@ const valida_produtos = () => {
     }
 }
 
+
+const valida_atualização = () => {
+    let nome_produto = document.getElementById('envia_nome').value
+    let preco_produto = document.getElementById('envia_preco').value
+    let atualiza_nome = document.getElementById('atualiza_nome').value
+    let atualiza_preco = document.getElementById('atualiza_preco').value
+
+    if(nome_produto && preco_produto && atualiza_nome && atualiza_preco !== ''){
+        for(let i in Produtos){        
+            if(Produtos[i]['nome'] == nome_produto.toUpperCase() && Produtos[i]['preco'] == preco_produto){
+                return [[Produtos[i]['nome'], Produtos[i]['preco'] , i] , [atualiza_nome.toUpperCase(), atualiza_preco]]    
+            }        
+        }
+        return alert('ERRO')
+    }else{
+        return alert('ERRO')
+    }         
+}
+
+
 const adiciona_produtos = (callback) => { 
     if(callback){               
         let request = {nome: callback[0], preco: callback[1]}
@@ -27,11 +46,12 @@ const adiciona_produtos = (callback) => {
     }    
 }
 
+
 const remove_produtos = (callback) => {    
     if(callback){        
         for(let i in Produtos){            
             if(Produtos[i]['nome'] === callback[0] && Produtos[i]['preco'] == callback[1]){                
-                delete Produtos[i]
+                Produtos.splice(i, 1)
                 console.log(Produtos)                                                                                         
             }            
         }
@@ -39,13 +59,32 @@ const remove_produtos = (callback) => {
     }    
 }
 
-//let i = 0
+
+const atualiza_produtos = (callback) => {
+    if(callback){               
+        Produtos[callback[0][2]]['nome'] = callback[1][0]
+        Produtos[callback[0][2]]['preco'] = callback[1][1]
+        mostra_produtos(Produtos, 0)
+    }    
+}
+
+
+const muda_display = () => {
+    let pega = document.getElementById('atualiza_produtos').style['display']       
+    if(pega === 'none'){
+        document.getElementById('atualiza_produtos').style.display = 'block'
+    }else{
+        document.getElementById('atualiza_produtos').style.display = 'none'
+    }
+}
+
+
 const mostra_produtos = (produtos, i) => {
     let teste = document.getElementById('tabelaDinamica').innerHTML = ''                                                                  
-    for(i ; i < produtos.length; i++){
-        console.log(i)        
+    for(i ; i < produtos.length; i++){            
         let teste = document.getElementById('tabelaDinamica').innerHTML += `<tr> <td>${i}</td> <td>${produtos[i]['nome']}</td> <td>${produtos[i]['preco']}</td></tr>`        
     }                
 }
+
 
 window.onload = mostra_produtos(Produtos, 0)
